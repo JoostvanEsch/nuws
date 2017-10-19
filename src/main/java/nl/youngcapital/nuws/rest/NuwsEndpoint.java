f7e4d83a84492f257b6b62b44388a7b179cb66cf
unknown <rw.emmens@gmail.com>
2017-10-19 09:17:52 +0200

    delete functies toegevoegd aan admin-app

--- src/main/java/nl/youngcapital/nuws/rest/NuwsEndpoint.java
+++ src/main/java/nl/youngcapital/nuws/rest/NuwsEndpoint.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,6 +68,20 @@ public class NuwsEndpoint {
         public String deleteNuws(){
 		System.out.println("endpoint werkt");
                 nuwsservice.deleteAllDatabase();
+                return "";
+        }
+        
+        @DeleteMapping("/deleteone/{id}")
+        public String deleteOne(@PathVariable long id){
+		System.out.println("endpoint werkt"+id);
+                nuwsservice.deleteOneFromDatabase(id);
+                return "";
+        }
+        
+        @DeleteMapping("/deletetag/{id}")
+        public String deleteTag(@PathVariable long id){
+		System.out.println("endpoint werkt"+id);
+                nuwsservice.deleteTagFromDatabase(id);
                 return "";
         }
         
@@ -151,8 +166,4 @@ public class NuwsEndpoint {
 	public void postCheckLogin(@RequestBody Gebruiker gebruiker){
 		//deze later nog aanvullen
 	}
-
-	
-	
-	
 }
